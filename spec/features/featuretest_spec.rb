f8e79a3b968a58087069df98e3753cb51903956c
Jacques <jacques.laffy@gmail.com>
2018-12-13 17:17:37 +0000

    Started creating form to input names - NOT WORKING YET

--- spec/features/featuretest_spec.rb
+++ spec/features/featuretest_spec.rb
@@ -1,6 +1,14 @@
 feature "Checking homepage" do
-  scenario "test" do
-    visit('/')
-    expect(page).to have_content "Testing infrastructure working!"
+  # scenario "test" do
+  #   visit('/')
+  #   expect(page).to have_content "Testing infrastructure working!"
+  # end
+
+  scenario "players enter lobby" do
+    visit ('/')
+    fill_in :names, with: "Jacques" "John"
+    click_button "Submit"
+    expect(page).to have_content "Jacques" "John"
   end
+
 end
